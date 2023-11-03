9ff45bba15ad5082fe2472e4beb885151e8d5cc5
Rhamses <rhamses.soares@gmail.com>
2023-11-03 11:37:25 -0300

    Updating Route file with new schema files.

--- src/db/routes.ts
+++ src/db/routes.ts
@@ -1,14 +1,20 @@
-import * as user from "./definitions/user";
-import * as post from "./definitions/post";
-import * as comment from "./definitions/comment";
-import * as category from "./definitions/category";
-import * as categoriesToPost from "./definitions/categoriesToPost";
+import * as user from "./definitions/users";
+import * as userMeta from "./definitions/usersMeta";
+import * as post from "./definitions/posts";
+import * as postMeta from "./definitions/postsMeta";
+import * as postTerm from "./definitions/postTerms";
+import * as term from "./definitions/terms";
+import * as termMeta from "./definitions/termsMeta";
+import * as option from "./definitions/options";
 
-import * as users from "./schema/users";
-import * as posts from "./schema/posts";
-import * as comments from "./schema/comments";
-import * as categories from "./schema/categories";
-import * as categoriesToPosts from "./schema/categoriesToPosts";
+import * as users from "./definitions/users";
+import * as usersMeta from "./definitions/usersMeta";
+import * as posts from "./definitions/posts";
+import * as postsMeta from "./definitions/postsMeta";
+import * as postsTerm from "./definitions/postTerms";
+import * as terms from "./definitions/terms";
+import * as termsMeta from "./definitions/termsMeta";
+import * as options from "./definitions/options";
 
 export interface ApiConfig {
   table: string;
@@ -18,17 +24,23 @@ export const apiConfig: ApiConfig[] = [];
 export const tableSchemaMap = new Map();
 export const tableDefinitions = [
   user,
+  userMeta,
   post,
-  comment,
-  category,
-  categoriesToPost,
+  postMeta,
+  postTerm,
+  term,
+  termMeta,
+  option,
 ];
 export const tableSchemas = [
   users,
+  usersMeta,
   posts,
-  comments,
-  categories,
-  categoriesToPosts,
+  postsMeta,
+  postsTerm,
+  terms,
+  termsMeta,
+  options,
 ];
 for (const [index, table] of tableDefinitions.entries()) {
   tableSchemaMap.set(table.tableName, tableSchemas[index].default);
