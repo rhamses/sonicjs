95467d01492c02900e6754d5d6c64b67171fbee6
Rhamses <rhamses.soares@gmail.com>
2023-11-27 22:09:23 -0300

    Creating helper file with global functions

---
+++ src/cms/util/helpers.ts
@@ -0,0 +1,41 @@
+import bcrypt from "bcrypt";
+
+export const savePassword = (password) => {
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(password, salt);
+  return hash;
+};
+
+export const slugify = (value: string, separator: string = "-"): string => {
+  const mapAccentsHex: any = {
+    a: /[\xE0-\xE6]/g,
+    A: /[\xC0-\xC6]/g,
+    e: /[\xE8-\xEB]/g,
+    E: /[\xC8-\xCB]/g,
+    i: /[\xEC-\xEF]/g,
+    I: /[\xCC-\xCF]/g,
+    o: /[\xF2-\xF6]/g,
+    O: /[\xD2-\xD6]/g,
+    u: /[\xF9-\xFC]/g,
+    U: /[\xD9-\xDC]/g,
+    c: /\xE7/g,
+    C: /\xC7/g,
+    n: /\xF1/g,
+    N: /\xD1/g,
+  };
+
+  for (let letter in mapAccentsHex) {
+    let regularExpression = mapAccentsHex[letter];
+    value = value.replace(regularExpression, letter);
+  }
+
+  value = value.toLowerCase();
+  value = value.replace(/[^a-z0-9\-]/g, " ");
+
+  value = value.replace(/ {2,}/g, " ");
+
+  value = value.trim();
+  value = value.replace(/\s/g, separator);
+
+  return value;
+};
