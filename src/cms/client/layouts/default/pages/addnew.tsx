4ebbb39c3ab9ca936128592dd1fac09cb1274334
Rhamses <rhamses.soares@gmail.com>
2024-01-06 18:22:50 -0300

    Update folder structure. Improving separation of concerns.

--- src/cms/client/layouts/default/pages/addnew.tsx
+++ src/cms/client/layouts/default/pages/addnew.tsx
@@ -1,14 +1,17 @@
 import { pageProps } from "../../../utils/interfaces"
 import { PageWrapper } from "../../../utils/page-wrapper";
-import { FormWrapper } from "../components/form/wrapper";
-import { FormItem } from "../components/form/item";
-import { Metadata } from "../components/form/metadata";
+import { FormWrapper } from "../components/form/components/form";
+import { FormInput } from "../components/form/inputs";
+import { MetaData } from "../components/form/components/metadata";
 export const AddNew = (props: pageProps) =>
-<PageWrapper action="Add new" pageName={props.pageName}>
-    <FormWrapper>
-      <FormItem id="title" type="text" label="Titulo" class="text-lg" />
-      <FormItem id="content" label="Conteúdo" type="textarea" />
-      <hr />
-      <Metadata />
-    </FormWrapper>
-</PageWrapper>
+  <PageWrapper action="Add new" pageName={props.pageName}>
+      <FormWrapper>
+        <FormInput id="post_title" type="text" label="Titulo" class="text-lg" />
+        <FormInput id="post_content" label="Conteúdo" type="textarea" />
+        <FormInput id="post_excerpt" label="Resumo" type="text" />
+        <FormInput id="post_slug" type="hidden" />
+        <FormInput id="post_type" type="hidden" value={props.pageName} />
+        <hr />
+        <MetaData />
+      </FormWrapper>
+  </PageWrapper>
--- src/cms/client/utils/page-wrapper.tsx
+++ src/cms/client/utils/page-wrapper.tsx
@@ -1,5 +1,5 @@
 import { pageProps } from "./interfaces"
-import { FormButton } from "../layouts/default/components/form/button"
+import { FormButton } from "../layouts/default/components/form/inputs"
 export const PageWrapper = (props: pageProps) =>
 <div class="bg-black/5 h-screen rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
   <header class="flex justify-between items-center mb-6">
