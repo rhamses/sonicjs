4c4c64f150e61dd38ec822c065edaa5fb6cf888d
Rhamses <rhamses.soares@gmail.com>
2024-01-23 17:15:48 -0300

    update frontend arquivos

--- public/js/client.js
+++ public/js/client.js
@@ -1,7 +1,18 @@
-function doAlert() {
-  alert("11sdasd");
+function deleteItem(el) {
+  const id = el.target.dataset.id;
+  const input = `${location.pathname}/${id}`;
+  fetch(input, {
+    method: "DELETE",
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.success) {
+        el.target.parentElement.parentElement.classList.add("hide");
+      }
+    })
+    .catch((err) => console.log("err", err));
 }
 
-document.querySelector("#metadataAdd").addEventListener("click", () => {
-  console.log("asdasdad");
-});
+document
+  .querySelectorAll(".btn-delete")
+  .forEach((item) => item.addEventListener("click", deleteItem));
