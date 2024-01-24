0c3d38490ac28099d252b501aba057d46347c927
Rhamses <rhamses.soares@gmail.com>
2024-01-24 15:01:16 -0300

    fix button configs to avoid intellisense conflicts

--- src/cms/client/layouts/default/components/form/inputs/templates/button.tsx
+++ src/cms/client/layouts/default/components/form/inputs/templates/button.tsx
@@ -1,7 +1,7 @@
 import { FormButton } from "../../../../interface"
 export default (props: FormButton) => { 
   const buttonCSS = "block text-white bg-meta-5 hover:bg-meta-5/40 focus:ring-4 focus:ring-meta-5/60 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 mt-5 dark:bg-meta-5/80 dark:hover:bg-meta-5/80 focus:outline-none dark:focus:ring-meta-5/80 " + props.class
-  let button = ""
+  let button;
   if (props.href) {
     button = <a id={ props.id } href={props.href} class={buttonCSS} >
       {props.label}
