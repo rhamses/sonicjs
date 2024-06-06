493503c28c1badda6b3c22d6298c69eb9c0af1e7
Rhamses <rhamses.soares@gmail.com>
2024-06-06 01:39:17 -0300

    adding new things

--- src/db/config-helpers.ts
+++ src/db/config-helpers.ts
@@ -27,36 +27,15 @@ export function isAdminOrUser(ctx: AppContext, id: string) {
   return isAdmin(ctx) || isUser(ctx, id);
 }
 
-export const slugify = (value: string, separator: string = '-'): string => {
-  const mapAccentsHex: any = {
-    a: /[\xE0-\xE6]/g,
-    A: /[\xC0-\xC6]/g,
-    e: /[\xE8-\xEB]/g,
-    E: /[\xC8-\xCB]/g,
-    i: /[\xEC-\xEF]/g,
-    I: /[\xCC-\xCF]/g,
-    o: /[\xF2-\xF6]/g,
-    O: /[\xD2-\xD6]/g,
-    u: /[\xF9-\xFC]/g,
-    U: /[\xD9-\xDC]/g,
-    c: /\xE7/g,
-    C: /\xC7/g,
-    n: /\xF1/g,
-    N: /\xD1/g
-  };
-
-  for (let letter in mapAccentsHex) {
-    let regularExpression = mapAccentsHex[letter];
-    value = value.replace(regularExpression, letter);
-  }
-
-  value = value.toLowerCase();
-  value = value.replace(/[^a-z0-9\-]/g, ' ');
-
-  value = value.replace(/ {2,}/g, ' ');
-
-  value = value.trim();
-  value = value.replace(/\s/g, separator);
-
-  return value;
+export const slugify = (text: string): string => {
+  return text
+    .toString() // Cast to string (optional)
+    .normalize('NFKD') // The normalize() using NFKD method returns the Unicode Normalization Form of a given string.
+    .toLowerCase() // Convert the string to lowercase letters
+    .trim() // Remove whitespace from both sides of a string (optional)
+    .replace(/\s+/g, '-') // Replace spaces with -
+    .replace(/[^\w\-]+/g, '') // Remove all non-word chars
+    .replace(/\_/g, '-') // Replace _ with -
+    .replace(/\-\-+/g, '-') // Replace multiple - with single -
+    .replace(/\-$/g, ''); // Remove trailing -
 };
